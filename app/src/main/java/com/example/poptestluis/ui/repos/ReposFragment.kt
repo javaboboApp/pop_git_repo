a2c6f9df3342b831d72dddb16f862beba2d87c15
luisernesto <ellokodeluis@gmail.com>
2020-08-11 19:17:18 +0200

    updated

--- app/src/main/java/com/example/poptestluis/ui/repos/ReposFragment.kt
+++ app/src/main/java/com/example/poptestluis/ui/repos/ReposFragment.kt
@@ -78,7 +78,7 @@ class ReposFragment : BaseFragment() {
     }
 
     private fun initGetRepos() {
-       //get the repositories of the for an user called square "
+       //get the repositories of the for an user called square
         lifecycleScope.launch {
 
             reposViewModel.getRepos("square").collectLatest { pagingGitRepository ->
